b2a052d241ed85b888d92ec322657764bd11e5e3
Asbert75 <erik_olsson_97@hotmail.com>
2023-09-08 17:40:17 +0200

    init

--- src/components/footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <nav className={['ml5', 'mr5', 'mt2', 'mb2'].join(" ")}>
-            <div>
-                <h2>Project<span>Zion</span></h2>
-            </div>
-        </nav>
-    );
-}
